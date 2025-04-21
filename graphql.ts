7738536f23bdbaf1d1f424969fd8624cc26bcabc
Kelly Davis <kldavis4@gmail.com>
2025-04-21 17:01:28 -0400

    add error mode

--- graphql.ts
+++ graphql.ts
@@ -13,12 +13,18 @@ export default async function handler(
   response.setHeader("x-serverless-function", request.url as string);
   response.setHeader("content-type", "application/json");
 
+  const error = request.url.includes("error") || !!request.headers["x-error"];
+
   if (crash) {
     response.statusCode = +request.headers["x-crash"] || 500;
     response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
     return response.json({
       message: `I crashed :( (${new Date().toTimeString()})`,
     });
+  } else if (error) {
+    response.statusCode = +request.headers["x-error"] || 500;
+    response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+    throw new Error(`I errored :( (${new Date().toTimeString()})`);
   } else {
     response.statusCode = 200;
     return response.json({
